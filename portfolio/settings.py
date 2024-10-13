17e579a5382f648282c48e2d9bfd569353abe65d
Mussy16 <musinachirevotafadzwa@gmail.com>
2024-10-13 21:38:35 +0200

    Collect static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+@0fb6nr0=_*+&e#0yh(!e2yx&2bf)b*o&(*_$mm&unyw8cfiy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
